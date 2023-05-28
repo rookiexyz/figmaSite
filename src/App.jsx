a9d20364b8189a3fe5e8b7912d54eb0114f8831b
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2023-05-28 17:04:12 +0500

    added services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Hero from './component/Hero';
 import Navbar from './component/Navbar';
+import Services from './component/Services';
 
 function App() {
   return (
     <div className="bg-site-blue text-secondary">
       <Navbar />
       <Hero />
-      {/* <h1 className='text-site-red font-bold text-8xl'>hello qasim</h1> */}
+      <Services />
     </div>
   );
 }
